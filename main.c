c9c58a4d4df958a17c6be1a59465a8627c16cb56
Meshkov Andrey <sonkezy@mail.ru>
2022-02-19 11:39:24 -0800

    Create function Show()

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Show(char c[8][8]){
+    printf("   ");
+    for(int i=0;i<8;i++){
+            printf(" %c",'a'+i);
+        }
+    printf(" \n");
+
+    printf("   -");
+    for(int i=0;i<8;i++){
+           printf("--");
+        }
+    printf("\n");
+
+    for(int i=7;i>=0;i--){
+        printf("%d |",i+1);
+        for(int j=0;j<8;j++){
+            printf(" %c",c[i][j]);
+        }
+        printf(" | %d \n",i+1);
+    }
+
+    printf("   -");
+    for(int i=0;i<8;i++){
+            printf("--");
+        }
+    printf("\n");
+
+    printf("   ");
+    for(int i=0;i<8;i++){
+            printf(" %c",'a'+i);
+        }
+    printf("\n");
+
+}
+
 int main()
 {
     char board[8][8]={
@@ -13,6 +48,7 @@ int main()
     {'p','p','p','p','p','p','p','p'},
     {'r','n','b','q','k','b','n','r'}
     };
+    Show(board);
     return 0;
 }
 
