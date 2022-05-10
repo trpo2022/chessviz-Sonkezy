7ccbd857066683a5f7638eaf50e6fdc9b50915ed
Meshkov Andrey <sonkezy@mail.ru>
2022-05-10 20:33:04 +0700

    Update codestyle

--- test/main.c
+++ test/main.c
@@ -2,7 +2,7 @@
 
 #include "../thirdparty/ctest.h"
 
-//Constant
+// Constant
 int w_pass = 0; // En passant for White
 int w_x = -1;   // Pawn possition for en passant
 int b_pass = 0; // En passant for Black
